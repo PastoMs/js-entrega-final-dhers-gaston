228d00523a47505d194a4ddfd6411ded4ec5d20b
PastoMs <gaston.dhers@gmail.com>
2022-12-16 17:32:58 -0300

    first commit

---
+++ productos.js
@@ -0,0 +1,11 @@
+let stock_prods = [
+    {id: 1, nombre: "Amazing Rare Jirachi", tipo: "Pokemon Card", cantidad: 1, precio: 4500, img: './img/jirachi.jpeg'},
+    {id: 2, nombre: "Amazing Rare Raikou", tipo: "Pokemon Card", cantidad: 1 , precio: 5000, img: './img/raikou.jpeg'},
+    {id: 3, nombre: "Amazing Rare Kyogre", tipo: "Pokemon Card", cantidad: 1, precio: 2500, img: './img/kyogre.jpeg'},
+    {id: 4, nombre: "Amazing Rare Zacian", tipo: "Pokemon Card", cantidad: 1, precio: 2000, img: './img/zacian.jpeg'},
+    {id: 5, nombre: "Amazing Rare Yveltal", tipo: "Pokemon Card", cantidad: 1, precio: 1500, img: './img/yveltal.jpeg'},
+    {id: 6, nombre: "Amazing Rare Reshiram", tipo: "Pokemon Card", cantidad: 1, precio: 3000, img: './img/reshiram.jpg'},
+    {id: 7, nombre: "Amazing Rare Celebi", tipo: "Pokemon Card", cantidad: 1, precio: 2800, img: './img/celebi.jpg'},
+    {id: 8, nombre: "Amazing Rare Rayquaza", tipo: "Pokemon Card", cantidad: 1, precio: 1800, img: './img/rayquaza.jpg'},
+    {id: 9, nombre: "Amazing Rare Zamazenta", tipo: "Pokemon Card", cantidad: 1, precio: 2700, img: './img/zamazenta.jpg'}   
+];
